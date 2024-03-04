aedbe4fdda7b1c570ccd33444043ad154f129fb0
yanxianhe <xianhe_yan@sina.com>
2024-03-04 17:38:07 +0800

    [fix](update restart-nginx.sh)

--- v2certbot/restart-nginx.sh
+++ v2certbot/restart-nginx.sh
@@ -2,10 +2,11 @@
 set -x
 # 获取证书到期天数
 EXPIRY_DATE=$(docker exec -it v2certbot certbot certificates |  grep "Expiry Date" | awk '{print $6}')
+DOMAIN_NAME=$(docker exec -it v2certbot certbot certificates | grep "Domains" | awk '{print $2}' | tr -d '\r')
 # 判断证书有效期，大于89天重新加载
 if [ $EXPIRY_DATE -ge 89 ]; then
-    sudo nginx -s reload 
-    # docker exec -it nginx nginx -s reload 
+    #sudo nginx -s reload 
+    docker exec -it nginx nginx -s reload 
     echo "Reload with validity period greater than 89 days"
 else
     echo "Certificate for ${DOMAIN_NAME} will expire on: ${EXPIRY_DATE}"
