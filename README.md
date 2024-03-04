c035250f244d3a79ed0e3fdf3ac6b3aec409dc5d
yanxianhe <xianhe_yan@sina.com>
2024-03-04 16:53:35 +0800

    [add](README.md)

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+# [v2certbot](./v2certbot/)
+
+## nginx 证书自动续签【certbot基于容器运行模式】
+
+- [基于Let’s Encrypt 推荐客户端](https://letsencrypt.org/zh-cn/docs/client-options/)
+
--- v2certbot/restart-nginx.sh
+++ v2certbot/restart-nginx.sh
@@ -2,9 +2,8 @@
 set -x
 # 获取证书到期天数
 EXPIRY_DATE=$(docker exec -it v2certbot certbot certificates |  grep "Expiry Date" | awk '{print $6}')
-# 判断证书有效期，大于89天不重新加载，否则续订
+# 判断证书有效期，大于89天重新加载
 if [ $EXPIRY_DATE -ge 89 ]; then
-    
     sudo nginx -s reload 
     # docker exec -it nginx nginx -s reload 
     echo "Reload with validity period greater than 89 days"
